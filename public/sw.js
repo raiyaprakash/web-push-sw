5e272e1634197a02ef6c7763275edb77216f921d
Prakash Raiya <59119176+raiyaprakash@users.noreply.github.com>
2025-06-18 05:46:58 +0530

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'asset-cache-v26',
+    ASSET_REGEX = /\.(?:jpe?g|png|gif|webp|avif|svg|bmp|ico|woff2?|ttf|otf|eot)$/i,
+    PRECACHE = ['/downloads/favicon.ico', '/manifest.json'],
+    MAX_ITEMS = 500;
+async function limitCache(cache) {
+    const keys = await cache.keys();
+    if (keys.length > MAX_ITEMS) return await cache.delete(keys[0]), limitCache(cache);
+}
+self.addEventListener('install', e => {
+    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(PRECACHE)).then(() => self.skipWaiting()));
+});
+self.addEventListener('fetch', e => {
+    if (e.request.method === 'GET' && ASSET_REGEX.test(e.request.url)) {
+        e.respondWith(caches.open(CACHE_NAME).then(async cache => {
+            const resp = await cache.match(e.request);
+            if (resp) return resp;
+            const r = await fetch(e.request);
+            cache.put(e.request, r.clone());
+            limitCache(cache);
+            return r;
+        }));
+    }
+});
+self.addEventListener('activate', e => {
+    e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+    }))).then(() => self.clients.claim()));
+});
