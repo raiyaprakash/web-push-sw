150345bfb9c3bd3f63d879eaeb372cb3f064fa1d
Prakash Raiya <59119176+raiyaprakash@users.noreply.github.com>
2026-03-03 07:58:53 +0530

    Update push-install.js

--- public/push-install.js
+++ public/push-install.js
@@ -1,14 +1,9 @@
-class LaraPush {
-    currentDateTime = Math.floor(new Date().getTime() / 1e3);
-    constructor(t, o) {
-        this.options = t, this.data = o, this.writeInLog("options", {
-            options: t
-        }), this.writeInLog("data", {
-            data: o
-        }), this.init()
+class AutoPush {
+    constructor(o) {
+        this.data = o, this.init()
     }
     writeInLog(...t) {
-        window.larapushLogger && console.log("LaraPush Log ::", ...t)
+        window.autopushLogger && console.log("AutoPush Log ::", ...t)
     }
     async waitForXSeconds(t) {
         return new Promise(o => {
@@ -31,7 +26,7 @@ class LaraPush {
                 console.error("Error:", a)
             }
             var i = await navigator.serviceWorker.register(location.origin + "/push-sw.js", {
-                scope: location.origin
+                scope: location.origin + "/"
             });
             return i.installing ? console.log("Service worker installing") : i.waiting ? console.log("Service worker installed") : i.active && console.log("Service worker active"), i
         } catch (e) {
@@ -39,22 +34,24 @@ class LaraPush {
         }
     };
     async initRegistration() {
-        if ("granted" == await this.getPermission() && null != localStorage.getItem("notification_token")) this.writeInLog("Token is already with larapush");
-        else {
-            await Notification.requestPermission();
-            var t = await this.registerServiceWorker();
-            this.writeInLog("sw", t);
-            let o = !1;
-            for (; !o;) await this.waitForXSeconds(.1), o = t.active;
-            let a = "";
-            a = document.querySelector("#larapush-custom-segment") ? document.querySelector("#larapush-custom-segment").value : window.location.href, t.active.postMessage({
-                command: "amp-web-push-subscribe",
-                url: a
-            })
+        await Notification.requestPermission();
+        const registration = await this.registerServiceWorker();
+        this.writeInLog("sw", registration);
+
+        let isActive = false;
+        while (!isActive) {
+            await this.waitForXSeconds(0.1);
+            isActive = registration.active;
         }
+
+        registration.active.postMessage({
+            command: "amp-web-push-subscribe",
+            url: window.location.href
+        });
     }
+
     async getPermission() {
-        var t = (await navigator.serviceWorker.getRegistrations()).filter(t => t.scope == location.origin);
+        var t = (await navigator.serviceWorker.getRegistrations()).filter(t => t.scope == location.origin + "/");
         let o = await Notification.permission;
         return "granted" == o ? 0 < t.length ? "granted" : "granted_default" : "default" == o ? "default" : "denied"
     }
@@ -68,142 +65,87 @@ class LaraPush {
             }
         })
     }
-    async notificationPermissionChangeHandler(t) {
-        this.writeInLog("Notification Permission Changed", t), this.popup("hide"), "enable" == this.data.lockPageContent && "granted" != t || this.backdrop("hide"), this.updateBottomButtonContent(t), this.updateBackdropContent(t), "granted" == t && this.initRegistration(), "granted_default" == t && navigator.serviceWorker.getRegistrations().then(t => {
-            for (var o of (this.writeInLog("Unsubscribed"), t)) o.unregister(), this.writeInLog("registration state", "unregistered"), this.writeInLog("registration unregistered", o)
-        })
+    async notificationPermissionChangeHandler(permission) {
+        this.writeInLog("Notification Permission Changed", permission);
+        if (permission === "granted") {
+            this.initRegistration();
+        }
+
+        if (permission === "granted_default") {
+            navigator.serviceWorker.getRegistrations().then((registrations) => {
+                this.writeInLog("Unsubscribed");
+                for (const reg of registrations) {
+                    reg.unregister();
+                    this.writeInLog("registration state", "unregistered");
+                    this.writeInLog("registration unregistered", reg);
+                }
+
+            });
+
+        }
     }
-    isMobile() {
-        return window.innerWidth <= 480
-    }
-    isDesktop() {
-        return !this.isMobile()
-    }
-    isCustomPopupIsEnabled() {
-        return !!(this.isMobile() && "enable" == this.data.mobile || this.isDesktop() && "enable" == this.data.desktop)
-    }
-    isPageLockEnabled() {
-        return "enable" == this.data.lockPageContent
-    }
+
     async requestPermission() {
-        this.popup("hide"), !this.isPageLockEnabled() && this.isCustomPopupIsEnabled() || this.backdrop("show"), this.isCustomPopupIsEnabled() || await this.waitForXSeconds(1), Notification.requestPermission().then(() => {
-            "enable" != this.data.lockPageContent && this.backdrop("hide")
-        })
+        await this.waitForXSeconds(1);
+        Notification.requestPermission().then(() => {
+
+        });
     }
     async showPopupAndRequestPermission() {
-        let t = async () => {
-            "default" == await this.getPermission() ? this.isCustomPopupIsEnabled() ? this.isPageLockEnabled() ? this.backdrop("show") : (null == await this.readData("notification_rejected") || this.currentDateTime - parseInt(await this.readData("notification_rejected")) > parseInt(this.data.reappear)) && (await this.waitForXSeconds(this.data.delay), "enable" == this.data.backdrop && this.backdrop("show"), this.popup("show")) : (await this.waitForXSeconds(this.data.delay), this.requestPermission()) : "granted" == await this.getPermission() ? this.initRegistration() : "denied" == await this.getPermission() ? "enable" == this.data.lockPageContent ? this.backdrop("show") : this.writeInLog("Notifications are blocked") : "granted_default" == await this.getPermission() && this.writeInLog("Notifications permissions granted denied manually")
+        let runLogic = async () => {
+            const permission = await this.getPermission();
+            if (permission === "default") {
+                await this.waitForXSeconds(this.data.delay);
+                this.requestPermission();
+            } else if (permission === "granted") {
+                this.initRegistration();
+            } else if (permission === "granted_default") {
+                this.writeInLog("Notifications permissions granted denied manually");
+            }
         };
-        "complete" === document.readyState ? await t() : document.onreadystatechange = async () => {
-            "complete" === document.readyState && await t()
+
+        if (document.readyState === "complete") {
+            await runLogic();
+        } else {
+            document.onreadystatechange = async () => {
+                if (document.readyState === "complete") {
+                    await runLogic();
+                }
+            };
         }
     }
-    async loadBottomButtonAndPoweredBy() {
-		
+    async isIOS() {
+        return !!navigator.userAgent.match(/iPad/i) || !!navigator.userAgent.match(/iPhone/i);
     }
-    async updateBottomButtonContent(t) {
-		
-	}
-    async updateBackdropContent(t) {
-		
-	}
-    async bottomButtonSidebox(t = "") {
-		
-	}
-    async bottomButtonTopbox(t = null, o = "") {
-		
-	}
-    async bottomButton(t = null) {
-		
-	}
-    getReferralUrl() {
-		
-	}
-    async backdrop(t = null) {
-		
-	}
-    async popup(t = null) {
-		
-	}
-    openDatabase = () => new Promise((t, o) => {
-        var a = indexedDB.open("autopushDataBase", 1);
-        a.onupgradeneeded = t => {
-            t.target.result.createObjectStore("myObjectStore", {
-                keyPath: "id"
-            })
-        }, a.onsuccess = o => {
-            t(o.target.result)
-        }, a.onerror = t => {
-            o(t.target.error)
-        }
-    });
-    writeData = async (t, o) => {
-        let a = (await this.openDatabase()).transaction("myObjectStore", "readwrite");
-        return a.objectStore("myObjectStore").put({
-            id: t,
-            data: o
-        }), new Promise((t, o) => {
-            a.oncomplete = () => {
-                t()
-            }, a.onerror = () => {
-                o(a.error)
-            }
-        })
-    };
-    readData = async t => {
-        let o = (await this.openDatabase()).transaction("myObjectStore", "readonly").objectStore("myObjectStore").get(t);
-        return new Promise((t, a) => {
-            o.onsuccess = () => {
-                t(o.result ? o.result.data : null)
-            }, o.onerror = () => {
-                a(o.error)
-            }
-        })
-    };
     async init() {
-        if (!window.LaraPushLoaded) {
-            window.LaraPushLoaded = !0, await this.registerServiceWorker();
-            let t = setInterval(async () => {
-                if (null != document.body) {
-                    clearInterval(t);
+        if (!window.AutoPushLoaded) {
+            window.AutoPushLoaded = true;
+            if (this.isIOS()) {
+                return;
+            }
+            await this.registerServiceWorker();
+            let interval = setInterval(async () => {
+                if (document.body != null) {
+                    clearInterval(interval);
                     try {
-                        this.loadBottomButtonAndPoweredBy(), this.setNotificationPermissionChangeListener(), (null == await this.readData("notification_rejected") || this.currentDateTime - parseInt(await this.readData("notification_rejected")) > parseInt(this.data.reappear)) && this.showPopupAndRequestPermission()
-                    } catch (o) {
-                        console.log(o)
+                        this.setNotificationPermissionChangeListener();
+                        this.showPopupAndRequestPermission();
+                    } catch (error) {
+                        console.log(error);
                     }
                 }
-            }, 100)
+            }, 100);
         }
     }
 }
-let larapushInterval = setInterval(() => {
-    "function" == typeof LoadLaraPush && (LoadLaraPush(), clearInterval(larapushInterval)), window.LaraPushLoaded && clearInterval(larapushInterval)
-}, 500);
 
-function LoadLaraPush() {
-    if ("function" == typeof LaraPush) try {
-        new LaraPush({
-            referralCode: "GHJKUY"
-        }, {
-            logo: null,
-            heading: null,
-            subheading: null,
-            themeColor: "#000000",
-            allowText: null,
-            denyText: null,
-            desktop: "disable",
-            mobile: "disable",
-            mobileLocation: null,
-            delay: "1",
-            reappear: "0",
-            bottomButton: "disable",
-            buttonToUnsubscribe: null,
-            lockPageContent: "disable",
-            backdrop: "enable",
-            popup_type: "default-prompt"
+function LoadAutoPush() {
+    if ("function" == typeof AutoPush) try {
+        new AutoPush({
+            delay: "1"
         })
     } catch (e) {
-        console.error("Failed to initialize LaraPush:", e)
-    } else console.error("LaraPush is not defined or not a function")
+        console.error("Failed to initialize AutoPush:", e)
+    } else console.error("AutoPush is not defined or not a function")
 }
-LoadLaraPush();
+LoadAutoPush();
